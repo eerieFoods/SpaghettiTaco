315248cf3d685e5d219badda1e47521b0f1fb8ce
Jan-Luca <jan-luca-wolf@web.de>
2023-11-15 10:25:49 +0100

    Added Guards, and Login stuff that doesnt work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { TodoComponent } from './components/todo/todo.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { AuthComponent } from './components/auth/auth.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    TodoComponent,
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/gun.service.ts
+++ src/app/services/gun.service.ts
@@ -4,6 +4,7 @@ import 'gun/sea';
 import 'gun/axe';
 import {GunCallbackUserAuth} from "gun/types/sea/GunCallbackUserAuth";
 import {environment} from "../../environments/environment.development";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root',
@@ -12,29 +13,43 @@ export class GunService {
   gun;
   user;
 
-  constructor() {
+  constructor(private router: Router) {
     this.gun = Gun(environment.DB_URL);
     this.user = this.gun.user().recall({ sessionStorage: true });
+    this.gun.on('auth', (x) => {
+      console.log(x);
+      console.log("Logged In?: " + this.isLoggedIn())
+      this.router.navigate(['/dashboard']).then(r => console.log(r));
+    });
   }
 
   createUser(username: string, password: string) {
-    this.user.create(username, password, (ack: GunCallbackUserCreate) => {
-      console.log(ack);
+    this.user.create(username, password, (ack: any) => {
+      if (ack.err) {
+        alert(ack.err);
+      } else {
+        this.signIn(username, password);
+      }
     });
   }
 
   signIn(username: string, password: string) {
     this.user.auth(username, password, (ack: GunCallbackUserAuth) => {
-        console.log(ack);
+      this.router.navigate(['/dashboard']).then(r => console.log(r));
     });
   }
 
   isLoggedIn() {
-    return this.user.is;
+    console.log("Id: " + this.user.id);
+    return this.user.id != undefined;
   }
 
   getFromUser(key: string) {
     return this.user.get(key);
+  }
+
+  signOut() {
+    this.user.leave();
   }
 
 }
